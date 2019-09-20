b5abd292a26bfb1d6c38b90cf2992b20e573813b
Alan Sato (TMNA) <alan.sato@toyota.com>
2019-09-20 15:02:56 -0500

    no dist on git

--- dist/models/coinbaseModels.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class cryptoPriceRequestJson {
-}
-exports.cryptoPriceRequestJson = cryptoPriceRequestJson;
-class DataField {
-}
-exports.DataField = DataField;
-//# sourceMappingURL=coinbaseModels.js.map
