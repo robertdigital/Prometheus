f0a541489b8d4516e07449405a87aa6f37e419db
Alan Sato (TMNA) <alan.sato@toyota.com>
2019-09-23 15:03:15 -0500

    change to use coinbase API client package

--- lib/index.ts
+++ lib/index.ts
@@ -1,33 +1,41 @@
-import { DataService } from "./services/DataService";
 import { Handler, Context, Callback } from "aws-lambda";
-import { cryptoPriceRequestJson } from "./models/coinbaseModels";
-import { DBController } from "./services/DBController";
+import { DBController } from "./controller/DBController";
 import { Db } from "mongodb";
 import { PriceDataModel } from "./models/cryptoPriceDataModels";
+import { Price, Client } from "coinbase";
+
+const API_KEY: string = process.env.API_KEY ? process.env.API_KEY : '';
+const API_SECRET: string = process.env.API_SECRET ? process.env.API_SECRET : '';
 
 let dbController: DBController | null = null;
 
 const handler: Handler = (event: any, context: Context, callback: Callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
-    let data = new DataService();
-    if (!dbController) {
-        dbController = new DBController();
-    }
-    data.getCurrentPrice().then((currentPriceString: string) => {
-        let currentPriceJSON: cryptoPriceRequestJson = JSON.parse(currentPriceString);
-        if (!dbController) {
-            dbController = new DBController();
+
+    let coinbaseClient: Client = new Client({ 'apiKey': API_KEY, 'apiSecret': API_SECRET });
+
+    // STEPS
+    // 1. Evaluate whether to buy
+    // - Evaluator class gets passed api client
+    // - makes needed calls and returns true or false if buy or not
+    // - maybe also find how much to buy
+    // 2. Execute the order
+    coinbaseClient.getBuyPrice({ 'currencyPair': 'BTC-USD' }, (error: Error, price: Price) => {
+        if(price){
+            if (!dbController) {
+                dbController = new DBController();
+            }
+            dbController.connectToDatabase().then(
+                (db: Db) => dbController.savePriceData(db, new PriceDataModel(parseFloat(price.data.amount))).then(
+                    (result: any) => {
+                        callback(null, result);
+                    }).catch((err: any) => {
+                        callback(err);
+                    })
+            ).catch((err: any) => {
+                callback(err);
+            });
         }
-        dbController.connectToDatabase().then(
-            (db: Db) => dbController.savePriceData(db, new PriceDataModel(parseFloat(currentPriceJSON.data.amount))).then(
-                (result: any) => {
-                    callback(null, result);
-                }).catch((err: any) => {
-                    callback(err);
-                })
-        ).catch((err: any) => {
-            callback(err);
-        });
     });
 }
 
--- lib/models/coinbaseModels.ts
+++
@@ -1,8 +0,0 @@
-export class cryptoPriceRequestJson {
-    public data : DataField; 
-}
-
-export class DataField {
-    public amount: string;
-    public currency: string;
-}
