28fc7f84a30049e15e73c2a395d8e21bcfcc45ce
Alan Sato (TMNA) <alan.sato@toyota.com>
2019-09-27 14:57:54 -0500

    Add RSI calculator

--- lib/utilities/TAUtils.ts
+++ lib/utilities/TAUtils.ts
@@ -1,4 +1,10 @@
-export class TechnicalAnalyzer{
+/**
+ * Utility class with functions to get technical indicators on a security.
+ *
+ * @export
+ * @class TechnicalAnalyzer
+ */
+export class TechnicalAnalyzer {
 
     /**
      * Gets the Exponential moving average of a set of values
@@ -9,18 +15,57 @@ export class TechnicalAnalyzer{
      * @returns {Array<number>} array of exponential moving averages
      * @memberof TechnicalAnalyzer
      */
-    public exponentialMovingAverage(prices: Array<number>,range:number):Array<number>{
+    public exponentialMovingAverage(prices: Array<number>, range: number): Array<number> {
         // prices come in with the most recent price in position 0
         // need to reverse array for calculation then re-reverse resulting array
         let reversedPrices = prices.reverse();
-        let k = 2/(range + 1);
+        let k = 2 / (range + 1);
         let emaArray = [reversedPrices[0]];
 
-        for (let i = 1; i < prices.length; i++){
-            emaArray.push(prices[i]*k + emaArray[i-1]*(1-k));
+        for (let i = 1; i < prices.length; i++) {
+            emaArray.push(prices[i] * k + emaArray[i - 1] * (1 - k));
         }
 
         emaArray = emaArray.reverse();
         return emaArray;
     }
+
+    //TO-DO: Documentation 
+    public relativeStrengthIndex(prices: Array<number>) {
+        let averageGain = this.averageChange(prices,true);
+        let averageLoss = this.averageChange(prices,false);
+
+        if(averageLoss === 0){
+            return 100;
+        }
+
+        let RSI: number = 100 - (100 / (1 + (averageGain/averageLoss)));
+
+        return RSI;
+    }
+
+    private averageChange(prices: Array<number>, gainsOrLosses: boolean) {
+        let reversedPrices = prices.reverse();
+        let amount: number = 0;
+
+        for (let i = 1; i < prices.length; i++) {
+            let change: number = reversedPrices[i] - reversedPrices[i - 1];
+            if (gainsOrLosses) {
+                if (change > 0) {
+                    amount += change;
+                }
+            } else {
+                if(change<0){
+                    amount += Math.abs(change);
+                }
+            }
+
+        }
+
+        return amount / prices.length;
+
+    }
+
+
+
 }
