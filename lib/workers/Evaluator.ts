53b5fb1ef5e72532c962d755b16b993f34ab8607
Alan Sato <sam.alan.sato@gmail.com>
2019-09-28 20:11:47 -0500

    Documentation attempt

--- lib/workers/Evaluator.ts
+++ lib/workers/Evaluator.ts
@@ -1,14 +1,29 @@
 import { TechnicalAnalyzer } from "../utilities/TAUtils";
 
 export class Evaluator {
+
     private ta = new TechnicalAnalyzer();
-    public async evaluatePrice(historicalData:any){
-        console.log("Currentprice according to hist : ",historicalData[0])
-        let ema10 = this.ta.exponentialMovingAverage(historicalData,10);
-        let ema20 = this.ta.exponentialMovingAverage(historicalData,20);
-        let ema50 = this.ta.exponentialMovingAverage(historicalData,50);
-        console.log("ema10: "+ ema10[0] + " || ema20: " + ema20[0] + " || ema50: "+ ema50[0]);
+
+    /**
+     * Main method of the evaluator. 
+     *  Full parameters not yet final. WIll take any necessary info
+     *  must take that info and make a decision whether or not to buy.
+     *  This decision may be preseted as a "decision" object that will comntain info
+     *  on whether to buy, sell  or do nothing, as well as how much to buy and sell in those cases.
+     *
+     * @param {*} historicalData
+     * @returns
+     * @memberof Evaluator
+     */
+    public async evaluatePrice(historicalData: any) {
+
+        console.log(historicalData[0]);
+        let ema10 = this.ta.exponentialMovingAverage(historicalData, 10);
+        let ema20 = this.ta.exponentialMovingAverage(historicalData, 20);
+        let ema50 = this.ta.exponentialMovingAverage(historicalData, 50);
+        console.log("ema10: " + ema10[0] + " || ema20: " + ema20[0] + " || ema50: " + ema50[0]);
         return true;
+
     }
 
 }
--- lib/workers/Executor.ts
+++ lib/workers/Executor.ts
@@ -1,5 +1,11 @@
 export class Executor{
-    public async executeOrder(){
+    /**
+     * Will take a decision from the evaluator and place the appropriate order on coinbase
+     *
+     * @returns
+     * @memberof Executor
+     */
+    public async executeDecision(){
         return true;
     }
 }
