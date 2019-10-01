bb9f44abc7202e585aede4311749fcb39013c235
Alan Sato (TMNA) <alan.sato@toyota.com>
2019-10-01 16:14:26 -0500

    Change API

--- lib/controllers/APIController.ts
+++ lib/controllers/APIController.ts
@@ -1,23 +1,31 @@
-import { Price, Client } from "coinbase";
+import { AuthenticatedClient } from "coinbase-pro"
 
 const API_KEY: string = process.env.API_KEY ? process.env.API_KEY : '';
 const API_SECRET: string = process.env.API_SECRET ? process.env.API_SECRET : '';
+const PASS: string = process.env.PASS_PHRASE ? process.env.PASS_PHRASE : '';
+const coinbaseProClient: AuthenticatedClient = new AuthenticatedClient(API_KEY,API_SECRET,PASS);
 
-const coinbaseClient: Client = new Client({ 'apiKey': API_KEY, 'apiSecret': API_SECRET });
 
+/**
+ * Controller to access the Coinbase PRO API (https://docs.pro.coinbase.com/)
+ * Contains methods to simplify the client requests.
+ * @export
+ * @class APIController
+ */
 export class APIController {
 
-    public getBuyPrice():Promise<Price>{
-        return new Promise( function(resolve,reject){
-            coinbaseClient.getBuyPrice({ 'currencyPair': 'BTC-USD' },(error:Error,priceObject: Price)=>{
-                if(error){
-                    reject(error);
-                } else {
-                    resolve(priceObject);
-                }
-            })
-        })
-            
+    /**
+     * gets the historical rate of a specified currency across a specified time period.
+     *
+     * @param {number} range days to go back
+     * @param {string} currency the currency to get rates for. (BTC-USD,ETH-USD,etc.)
+     * @returns PROMISE[ [ time, low, high, open, close, volume ], ...]
+     * @memberof APIController
+     */
+    public getHistoricRatesByDay(range:number,currency:string) {
+        let currentDate = new Date();
+        let periodDate = new Date(new Date().setDate(currentDate.getDate() - range));
+        return coinbaseProClient.getProductHistoricRates(currency,{start:periodDate.toISOString(),end:currentDate.toISOString(),granularity:86400});
     }
 
 }
--- lib/index.ts
+++ lib/index.ts
@@ -12,58 +12,18 @@ let executor: Executor | null = null;
 
 const handler: Handler = (event: any, context: Context, callback: Callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
-    let date: Date = new Date(event.time);
-    console.log(date.getHours());
-    console.log(date.getMinutes());
-    if(date.getHours()){
-        console.log(date.getHours()==1);
-    }
-    let timeMark:number = date.getHours() != 0 ? date.getHours()*2 : 48;
-    timeMark = date.getMinutes()== 0 ? timeMark - 1 : timeMark;
-    console.log(timeMark);
+    
     if (!apiController) {
         apiController = new APIController();
     }
     if (!dbController) {
         dbController = new DBController();
     }
-    // STEPS
-    // 1. Get Price
-    // - Store Price in DB
-    // - retrieve historical data
-    // 2. Using current price and historical data, evaluate whether to buy
-    // - Runs technical analysis formulas to determine whether to buy
-    // - maybe also find how much to buy
-    // 3. Execute the order
-    // - Save transaction in DB
 
-    Promise.all([dbController.connectToDatabase(), apiController.getBuyPrice()])
-        .then((dbAndPrice: Array<any>) => {
-            //Preliminary info needed 
-            return dbController.savePriceData(dbAndPrice[0], new PriceDataModel(parseFloat(dbAndPrice[1].data.amount),timeMark)).then((price) => {
-                return dbController.getHistoricalData(dbAndPrice[0],250);
-            })
-        })
-        .then((historicalData: Array<number>) => {
-            // Evaluator
-            if (!evaluator) {
-                evaluator = new Evaluator();
-            }
-            return evaluator.evaluatePrice(historicalData);
-        })
-        .then((res) => {
-            // Executor
-            if (!executor) {
-                executor = new Executor();
-            }
-            return executor.executeDecision();
-        })
-        .then((res) => {
-            callback(null, "True or false :: Sam is the coolest man in the world : " + res);
-        })
-        .catch((error) => {
-            callback(error);
-        })
+    apiController.getHistoricRatesByDay(10,'BTC-USD').then((res)=>{
+        console.log("the data",res);
+        callback(null,"done");
+    });
 
 }
 
