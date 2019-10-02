62ab14dde54ab55ae8eb57dc69ad1be4e961d04e
Alan Sato (TMNA) <alan.sato@toyota.com>
2019-10-02 15:41:39 -0500

    implement fibinocci retracements

--- lib/utilities/TAUtils.ts
+++ lib/utilities/TAUtils.ts
@@ -53,18 +53,18 @@ export class TechnicalAnalyzer {
      * @returns
      * @memberof TechnicalAnalyzer
      */
-    public historicEMA({ values, range, base = 4 }: { values: Array<Array<number>>; range: number; base?: number; }):Array<number>{
-        
+    public historicEMA({ values, range, base = 4 }: { values: Array<Array<number>>; range: number; base?: number; }): Array<number> {
+
         let slimmedArray: Array<number> = [];
-        for(let i = 0; i<values.length;i++){
+        for (let i = 0; i < values.length; i++) {
             slimmedArray.push(values[i][base]);
         }
-        return this.ema(slimmedArray,range);
+        return this.ema(slimmedArray, range);
     }
 
     //TO-DO: Documentation 
     // Method to return the rsi(Relative Strength Index) of a set of prices
-    public rsi(values: Array<number>, range: number):number {
+    public rsi(values: Array<number>, range: number): number {
         let averageGain = this.averageChange(values, range, true);
         let averageLoss = this.averageChange(values, range, false);
 
@@ -77,7 +77,7 @@ export class TechnicalAnalyzer {
         return RSI;
     }
 
-    private averageChange(values: Array<number>, range: number, gainsOrLosses: boolean):number {
+    private averageChange(values: Array<number>, range: number, gainsOrLosses: boolean): number {
         let reversedValues = values.reverse();
         let amount: number = 0;
 
@@ -99,7 +99,7 @@ export class TechnicalAnalyzer {
 
     }
 
-    public macd(values: Array<number>, range):Array<number> {
+    public macd(values: Array<number>, range): Array<number> {
         let macds: Array<number> = [];
         for (let i = 0; i < range; i++) {
             macds.push(this.ema(values, 12)[i] - this.ema(values, 26)[i]);
@@ -107,7 +107,7 @@ export class TechnicalAnalyzer {
         return macds;
     }
 
-    public macdSignal(macdValues: Array<number>):Array<number> {
+    public macdSignal(macdValues: Array<number>): Array<number> {
         return this.ema(macdValues, 9);
     }
 
@@ -124,18 +124,47 @@ export class TechnicalAnalyzer {
     public srsi(values: Array<number>, period: number): number {
         // stoch rsi = (rsi - lowestrsi)/(highestrsi - lowestrsi)
         //find rsi for each day of the array for the required period
-        if(values.length<(period*2)){
+        if (values.length < (period * 2)) {
             return;
         }
-        let rsiList:Array<number> = [];
-        for(let i = 0; i<period; i++){
-            rsiList.push(this.rsi(values.slice(i,period+i),period));
+        let rsiList: Array<number> = [];
+        for (let i = 0; i < period; i++) {
+            rsiList.push(this.rsi(values.slice(i, period + i), period));
         }
         let high = Math.max(...rsiList);
         let low = Math.min(...rsiList);
-        return ((rsiList[0]-low)/(high-low));
+        return ((rsiList[0] - low) / (high - low));
     }
 
+    /**
+     * Gets the fibanocci retracement levels for a given value range.
+     * levels calculated are for:
+     * 
+     *  (0, 23.6, 38.2, 50, 61.8, 78.6, 100)
+     *
+     * @param {number} start
+     * @param {number} end
+     * @returns {Array<number>}
+     * @memberof TechnicalAnalyzer
+     */
+    public fibRetracements(start: number, end: number): Array<number> {
+        let levels: number[] = [0, 23.6, 38.2, 50, 61.8, 78.6, 100];
+        let retLevels: number[];
+
+        if (start < end) {
+            retLevels = levels.map((level: number) => {
+                let lvl: number = end - (Math.abs(start - end) * (level / 100))
+                return lvl;
+            })
+        } else {
+            retLevels = levels.map((level: number) => {
+                let lvl: number = end + (Math.abs(start - end) * (level / 100))
+                return lvl;
+            })
+        }
+
+        return retLevels;
+    }
 
 
 }
