1001c9782660e6fdb9474d52bf5c8453dfca66ff
Alan Sato (TMNA) <alan.sato@toyota.com>
2019-10-04 15:12:06 -0500

    MACD evaluation start

--- lib/models/DatabaseModels.ts
+++ lib/models/DatabaseModels.ts
@@ -1,18 +1,34 @@
-
-/**
- * Model used for storing the current price of a currency to the database.
- *
- * @export
- * @class PriceDataModel
- */
-export class PriceDataModel {
+export class Evaluation {
     public date: Date;
-    public price: number;
-    public timeMark: number;
+    public macdStatus: MACDStatus;
+    public macdCrossoverSignal: boolean;
+    constructor(macD: Array<number>, signal: Array<number>, lastEval?: Evaluation){
+        
+        this.macdStatus = lastEval ? new MACDStatus(macD,signal,lastEval.macdStatus) : new MACDStatus(macD,signal);
+        this.macdCrossoverSignal = lastEval? (lastEval.macdStatus.macdGTSignal != (macD[0]>signal[0])) : false;
 
-    constructor(p: number, mark: number) {
         this.date = new Date();
-        this.price = p;
-        this.timeMark = mark;
+    }
+}
+
+export class MACDStatus {
+    public lastMacDs: Array<number>;
+    public lastSignals: Array<number>;
+    public macdGTSignal: boolean;
+    public converging: boolean;
+    constructor(macD: Array<number>, signal: Array<number>, lastIndDrirecton?: MACDStatus) {
+        if (lastIndDrirecton) {
+            if (lastIndDrirecton.lastMacDs) {
+                if (Math.abs(macD[0] - signal[0]) < Math.abs(lastIndDrirecton.lastMacDs[0] - lastIndDrirecton.lastSignals[0])) {
+                    //the distance between the macD and signal is shrinking
+                    this.converging = true;
+                } else {
+                    this.converging = false;
+                }
+            }
+        }
+        this.lastMacDs = macD;
+        this.lastSignals = signal;
+        this.macdGTSignal = macD[0] > signal[0];
     }
 }
--- lib/workers/Evaluator.ts
+++ lib/workers/Evaluator.ts
@@ -1,5 +1,6 @@
 import { TechnicalAnalyzer } from "../utilities/TAUtils";
 import { ProductTicker } from "coinbase-pro";
+import { MACDStatus, Evaluation } from "../models/DatabaseModels";
 
 export class Evaluator {
 
@@ -16,13 +17,22 @@ export class Evaluator {
      * @returns
      * @memberof Evaluator
      */
-    public async evaluatePrice(ticker: ProductTicker, orderBook: Array<any>, historicalData: Array<Array<number>>) {
+    public async evaluatePrice(ticker: ProductTicker, orderBook: Array<any>, historicalData: Array<Array<number>>,lastEval:Evaluation) {
         // console.log("hello")
         let ema10 = this.ta.historicEMA({ values: historicalData, range: 10 });
         let ema20 = this.ta.historicEMA({ values: historicalData, range: 20 });
         let ema50 = this.ta.historicEMA({ values: historicalData, range: 50 });
         console.log("ema10: " + ema10[0] + " || ema20: " + ema20[0] + " || ema50: " + ema50[0]);
-        return true;
+
+        let macd = this.ta.macd(historicalData,9);
+        let signalLine = this.ta.macdSignal(macd);
+        console.log("lastEval",lastEval);
+        console.log("macDs",macd);
+        console.log("CurrentMacD",macd[0])
+        console.log("signalline",signalLine);
+        console.log("CurrentSignal",signalLine[0])
+        let evaluation = new Evaluation(macd,signalLine,lastEval);
+        return evaluation;
     }
 
 }
