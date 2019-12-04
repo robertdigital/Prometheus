889f01d93e65be2c42ab1e214630ef592eccac99
Alan Sato (TMNA) <alan.sato@toyota.com>
2019-12-04 09:44:56 -0600

    basic constants addition

---
+++ lib/constants/constants.ts
@@ -0,0 +1,7 @@
+export const BTC: string = 'BTC';
+export const USD: string = 'USD';
+export const BTCUSD: string = 'BTC-USD';
+export const MARKETORDER: string = 'market';
+export const LIMITORDER: string = 'limit';
+export const BUY: string = 'buy';
+export const SELL: string = 'sell';
