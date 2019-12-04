128334e849197e41e2f0af54ce741b916cdeebfc
Alan Sato (TMNA) <alan.sato@toyota.com>
2019-12-04 15:51:28 -0600

    update test

--- tests/ta.test.ts
+++ tests/ta.test.ts
@@ -3,19 +3,19 @@ import 'mocha';
 
 import { TechnicalAnalyzer } from '../dist/utilities/TAUtils';
 
-describe('Simple Moving Average', function() {
-    it('returns average', function() {
+describe('Simple Moving Average', function () {
+    it('returns average', function () {
         let result = new TechnicalAnalyzer().sma([10, 11, 12], 3);
         expect(result).equal(11);
     });
 });
 
-describe('Exponential Moving Average', function() {
-    it('returns exponential average', function() {
+describe('Exponential Moving Average', function () {
+    it('returns exponential average', function () {
         let result = new TechnicalAnalyzer().ema(
             [1.5554, 1.5555, 1.5558, 1.556],
             4
         );
-        expect(result[0]).equal(1.5556112);
+        expect(result[0]).equal(1.555541);
     });
 });
