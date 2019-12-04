b91896ddea5c72433727de11e11751b5d01bd673
Alan Sato (TMNA) <alan.sato@toyota.com>
2019-12-04 11:08:32 -0600

    More constants

--- lib/constants/constants.ts
+++ lib/constants/constants.ts
@@ -5,3 +5,11 @@ export const MARKETORDER: string = 'market';
 export const LIMITORDER: string = 'limit';
 export const BUY: string = 'buy';
 export const SELL: string = 'sell';
+
+
+export const USDMINIMUM: number = 10;
+export const USDMAXIMUM: number = 1000000;
+export const USDPRECISION: number = 2;
+export const BTCMINIMUM: number = 0.001;
+export const BTCMAXIMUM: number = 280.00000000;
+export const BTCPRECISION: number = 8;
